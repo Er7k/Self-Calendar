19ae54409991495fce676513024f2610427e2396
LovisaPaulina <lovisapaulina02@gmail.com>
2025-04-17 00:20:56 +0200

    Har gjort knappar för dag, vecka, månad, år och en to-do list och js kod till den som gör att man kan lägga till en task

---
+++ src/main/resources/static/js/toDoListJS.js
@@ -0,0 +1,19 @@
+
+document.getElementById('addTaskButton').addEventListener('click', function() {
+    const toDoList = document.getElementById('toDoItems');
+    const taskText = prompt("Enter new task: ");
+
+    if (taskText && taskText.trim() !== "") {
+        const addTask = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+
+        addTask.appendChild(checkbox);
+        addTask.appendChild(document.createTextNode(' ' + taskText));
+
+        toDoList.appendChild(addTask);
+    } else {
+        alert("Task cannot be empty!");
+    }
+
+});
