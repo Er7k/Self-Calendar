8bb35eecfe8c6cfa717983c5f3d5d2ba66b97a74
SimonLjung123 <simon.ljung2001@gmail.com>
2025-04-25 13:16:32 +0200

    Lagt till klasser

---
+++ src/main/java/com/g25/selfcalendar/Entity/RecurringActivity.java
@@ -0,0 +1,24 @@
+//package com.g25.selfcalendar.Entity;
+//
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//
+//@Entity
+//public class RecurringActivity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//}
+//
+//Kommentera tillbaka när vi har en databas kopplad
