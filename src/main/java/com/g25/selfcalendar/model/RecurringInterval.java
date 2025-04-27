d5a339e8dd361791b7fcb72b56685c51af7c312f
fridalarsson13 <fridalarsson25@gmail.com>
2025-04-27 10:28:46 +0200

    de borde funka nu

--- src/main/java/com/g25/selfcalendar/model/RecurringInterval.java
+++ src/main/java/com/g25/selfcalendar/model/RecurringInterval.java
@@ -4,6 +4,8 @@ package com.g25.selfcalendar.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Getter // slippa skriva alla metoder
 @Setter // slippa skriva alla metoder
 @NoArgsConstructor // slippa skriva constructor
@@ -18,11 +20,6 @@ public class RecurringInterval {
     private Long id;
 
 
-    // en till många relationship en recurring interval till många events
-    @OneToMany(fetch = FetchType.LAZY) // lazy = User object will be loaded lazily (only when needed)
-    // kan bytas till .EAGER = load the User immediately when the Event is loaded
-    @JoinColumn(name = "event_id")
-    private Event event; // foregin key user
 
 
 }
